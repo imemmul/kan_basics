ab87d35330f1ccd9ebb1b075eee65c9981a81ddf
Emir Ulurak <emir.ulurak@ozu.edu.tr>
2024-07-22 03:22:04 +0300

    optimized conv aroudn 100x speedup

--- kanvisiontorch/convs/kan_conv.py
+++ kanvisiontorch/convs/kan_conv.py
@@ -1,5 +1,7 @@
-from kans import KAN
+from kans import KAN, KANLayer
+import time
 import torch.nn as nn
+import matplotlib.pyplot as plt
 import torch
 import torch.nn.functional as F
 
@@ -9,20 +11,18 @@ class KANClassification(nn.Module):
         self.device = device
         self.conv_layers = nn.ModuleList([
             ConvKANLayer(input_channel, 2, 3, device=self.device),
-            nn.MaxPool2d(3),
+            nn.AvgPool2d(3),
             ConvKANLayer(2, 2, 3, device=self.device),
+            nn.BatchNorm2d(2),
         ]).to(self.device)
 
-        self.kan_network = KAN([72, 36, n_classes], device=self.device)
+        self.kan_network = KAN([128, 64, n_classes], device=self.device)
 
     def forward(self, x):
-        for layer in self.conv_layers:
-            # print(f"before layer: {x.shape}")
+        for i, layer in enumerate(self.conv_layers):
+            # print(f"Conv layer {i}: ")
             x = layer(x)
-            # print(f"after layer: {x.shape}")
-        # x = x.view(x.size(0), -1)  # Flatten the tensor
         x = x.view(x.size(0), -1)
-        # print(x.shape)
         x = self.kan_network(x)
         return x
 
@@ -36,12 +36,35 @@ class ConvKANLayer(nn.Module):
         self.stride = stride
         self.padding = padding
         self.kan_layers = nn.ModuleList()
-
         for i in range(out_channels):
-            self.kan_network = KAN([in_channels * kernel_size * kernel_size, 1], device=self.device)
-            self.kan_layers.append(self.kan_network)
-
+            kan_networks = KAN([in_channels * kernel_size * kernel_size, in_channels * kernel_size * kernel_size], device=self.device)
+            self.kan_layers.append(kan_networks)
+            
     def forward(self, x):
+        x = x.to(self.device)
+        batch_size, in_channels, in_height, in_width = x.shape
+
+        if self.padding > 0:
+            x = F.pad(x, (self.padding, self.padding, self.padding, self.padding))
+
+        out_height = (in_height - self.kernel_size + 2 * self.padding) // self.stride + 1
+        out_width = (in_width - self.kernel_size + 2 * self.padding) // self.stride + 1
+        x_unf = F.unfold(x, kernel_size=self.kernel_size, stride=self.stride)
+        x_unf = x_unf.view(batch_size, in_channels, self.kernel_size, self.kernel_size, -1)
+        x_unf = x_unf.permute(0, 4, 1, 2, 3).contiguous()
+        x_unf = x_unf.view(batch_size * x_unf.size(1), -1)
+        out = torch.zeros(batch_size, self.out_channels, out_height * out_width, dtype=torch.float32, device=self.device)
+        for o in range(self.out_channels):
+            kan_out = self.kan_layers[o](x_unf).view(batch_size, out_height * out_width, -1)
+            kan_out = torch.sum(kan_out, dim=-1)
+            out[:, o, :] = kan_out
+        out = out.view(batch_size, self.out_channels, out_height, out_width)
+        return out
+    
+    def forward_deprecated(self, x):
+        """
+        shitty conv below, behold !
+        """
         x = x.to(self.device)
         batch_size, in_channels, in_height, in_width = x.shape
         if self.padding > 0:
@@ -52,13 +75,23 @@ class ConvKANLayer(nn.Module):
 
         out = torch.zeros((batch_size, self.out_channels, out_height, out_width), dtype=torch.float32, device=self.device)
         self.xin_cache = x
-
+        # total_time_kan_forward = 0
+        # total_time_conv_kan = time.time()
+        print(f"how many loops are there below: {batch_size * self.out_channels * (in_height - self.kernel_size + 1) * (in_width - self.kernel_size + 1)}")
         for b in range(batch_size):
             for o in range(self.out_channels):
                 for i in range(0, in_height - self.kernel_size + 1, self.stride):
                     for j in range(0, in_width - self.kernel_size + 1, self.stride):
                         region = x[b, :, i:i + self.kernel_size, j:j + self.kernel_size]
-                        out[b, o, i // self.stride, j // self.stride] = self.kan_layers[o](region.flatten().unsqueeze(0))
-
+                        # conv_forward = time.time()
+                        # print(f"region shape: {region.shape}, kan shape: {region.flatten().unsqueeze(0).shape}")
+                        # print(f"output_kan shape: {output_kan.shape}, torch.sum(output_kan): {torch.sum(output_kan).shape}")
+                        
+                        out[b, o, i // self.stride, j // self.stride] = torch.sum(self.kan_layers[o](region.flatten().unsqueeze(0)))
+                        # conv_forward_time = time.time() - conv_forward
+                        # total_time_kan_forward += conv_forward_time
+        
+        # total_time_conv_kan = time.time() - total_time_conv_kan
+        # print(f"All time: {total_time_conv_kan}, total time taken for kan forward: {total_time_kan_forward}")
         self.output = out
         return out
--- kanvisiontorch/train_fmnist.py
+++ kanvisiontorch/train_fmnist.py
@@ -1,38 +1,14 @@
 import hydra
 from convs import KANClassification
 import torch.nn as nn
+from kans import KAN
 from torchvision import transforms
 import torch
-from torchvision.datasets import FashionMNIST
+from torchvision.datasets import FashionMNIST, CIFAR10
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 import time
 import os
-
-def train_model_basic(model, train_loader, val_loader, optimizer, loss_func, cfg, epoch):
-    model.train()
-    running_loss = 0.0
-    pbar = tqdm(train_loader, desc="Training")
-    for images, labels in pbar:
-        start = time.time()
-        images, labels = images.to(cfg.device), labels.to(cfg.device)
-    
-        optimizer.zero_grad()
-        outputs = model(images)
-        loss = loss_func(outputs, labels)
-        loss.backward()
-        optimizer.step()
-        pbar.set_postfix_str(f"Loss: {loss.item():.4f}")
-        running_loss += loss.item() * images.size(0)
-        end = time.time()
-        # print(f"Time taken: {end - start:.2f}s")
-
-    epoch_loss = running_loss / len(train_loader.dataset)
-    print(f"Epoch {epoch+1} Training Loss: {epoch_loss:.4f}")
-
-    # Save the model at specified intervals
-    if (epoch + 1) % cfg.save_interval == 0 or (epoch + 1) == cfg.epochs:
-        save_model(model, cfg, epoch)
 
 def evaluate_basic(model, test_loader, criterion, device):
     model.eval()
@@ -64,30 +40,37 @@ def save_model(model, cfg, epoch):
 def load_dataset(dataset_path, batch_size):
     transform = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize((0.5,), (0.5,))
     ])
 
-    train_dataset = FashionMNIST(root=dataset_path, train=True, download=True, transform=transform)
-    train_dataset_subset = torch.utils.data.Subset(train_dataset, range(0, 10000))
-    
-    test_dataset = FashionMNIST(root=dataset_path, train=False, download=True, transform=transform)
-    test_dataset_subset = torch.utils.data.Subset(test_dataset, range(0, 1000))
+    # train_dataset = FashionMNIST(root=dataset_path, train=True, download=True, transform=transform)
+    # test_dataset = FashionMNIST(root=dataset_path, train=False, download=True, transform=transform)
+    train_dataset = CIFAR10(root=dataset_path, train=True, download=True, transform=transform)
+    test_dataset = CIFAR10(root=dataset_path, train=False, download=True, transform=transform)
 
-    # train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, pin_memory=True)
-    # test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, pin_memory=True)
-    train_dataloader = DataLoader(train_dataset_subset, batch_size=batch_size, shuffle=True, pin_memory=True)
-    test_dataloader = DataLoader(test_dataset_subset, batch_size=batch_size, shuffle=False, pin_memory=True)
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, pin_memory=True)
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, pin_memory=True)
     
     return train_dataloader, test_dataloader
 
+def check_gradients(model):
+    for name, param in model.named_parameters():
+        if param.grad is not None:
+            print(f"{name} grad mean: {param.grad.mean().item()}, std: {param.grad.std().item()}")
+        else:
+            print(f"{name} grad is None")
+
+
 @hydra.main(config_path='./configs', config_name='fmnist.yaml')
 def main(cfg):
-    model = KANClassification(input_channel=1, n_classes=10, height=28, width=28, device=cfg.device)
+    # model = KAN(layers_shape=[784, 512, 256, 128, 64, 32, 16, 10], device=cfg.device)
+    model = KANClassification(input_channel=3, n_classes=10, height=32, width=32, device=cfg.device)
     model.to(cfg.device)
     train_loader, test_loader = load_dataset(cfg.dataset_path, cfg.train_batch_size)
-    loss = nn.CrossEntropyLoss(label_smoothing=cfg.loss.label_smoothing)
+    loss_func = nn.CrossEntropyLoss(label_smoothing=cfg.loss.label_smoothing)
     optimizer_class = torch.optim.AdamW
     params_to_optimize = model.parameters()
+    for name, param in model.named_parameters():
+        print(f"{name} requires grad: {param.requires_grad}")
     optimizer = optimizer_class(
         params_to_optimize,
         lr=cfg.optim.learning_rate,
@@ -95,12 +78,31 @@ def main(cfg):
         weight_decay=cfg.optim.adam_weight_decay,
         eps=cfg.optim.adam_epsilon,
     )
-
+    model.train()
     for epoch in range(cfg.epochs):
-        print(f"Epoch {epoch+1}/{cfg.epochs}")
-        train_model_basic(model=model, train_loader=train_loader, val_loader=test_loader, optimizer=optimizer, loss_func=loss, cfg=cfg, epoch=epoch)
-        if epoch % 50 == 0:
-            evaluate_basic(model=model, test_loader=test_loader, criterion=loss, device=cfg.device)
+        running_loss = 0.0
+        pbar = tqdm(train_loader, desc="Training")
+        for images, labels in pbar:
+            start = time.time()
+            images, labels = images.to(cfg.device), labels.to(cfg.device)
+            optimizer.zero_grad()
+            outputs = model(images)
+            loss = loss_func(outputs, labels)
+            loss.backward()
+            # check_gradients(model)
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+            optimizer.step()
+            pbar.set_postfix_str(f"Loss: {loss.item():.4f}")
+            running_loss += loss.item() * images.size(0)
+            end = time.time()
+
+        epoch_loss = running_loss / len(train_loader.dataset)
+        print(f"Epoch {epoch+1} Training Loss: {epoch_loss:.4f}")
+
+        if (epoch + 1) % cfg.save_interval == 0 or (epoch + 1) == cfg.epochs:
+            save_model(model, cfg, epoch)
+            if epoch % 2 == 0:
+                evaluate_basic(model=model, test_loader=test_loader, criterion=loss_func, device=cfg.device)
 
 if __name__ == "__main__":
     main()
